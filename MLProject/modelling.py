aa93d425ffe264ee82190898852084786d997d8f
Attaboy473 <attilla.ghozali@gmail.com>
2026-01-04 16:43:39 +0700

    Add files via upload

---
+++ MLProject/modelling.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+mlflow.set_experiment("CI-Training")
+
+df = pd.read_csv("train_preprocessed.csv")
+
+X = df.drop("price_range", axis=1)
+y = df["price_range"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+with mlflow.start_run():
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
