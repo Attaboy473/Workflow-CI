f3d30caf92fb9a667b97b4f2af905c78ed8aff95
Attaboy473 <attilla.ghozali@gmail.com>
2026-01-04 18:03:55 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,27 +1,31 @@
 import mlflow
 import mlflow.sklearn
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-mlflow.set_experiment("CI-Training")
-
-# load dataset
+# Load dataset
 df = pd.read_csv("train_preprocessed.csv")
 
-X = df.drop("target", axis=1)
-y = df["target"]
+TARGET_COL = "label"
+
+X = df.drop(TARGET_COL, axis=1)
+y = df[TARGET_COL]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
+with mlflow.start_run():
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
 
-mlflow.log_metric("accuracy", acc)
-mlflow.sklearn.log_model(model, "model")
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
+
+    print(f"Accuracy: {acc}")
