760e1e58eee26cc196e8f512e02efe3e8578f71e
Attaboy473 <attilla.ghozali@gmail.com>
2026-01-04 17:55:05 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,31 +1,27 @@
-import pandas as pd
 import mlflow
 import mlflow.sklearn
-
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 mlflow.set_experiment("CI-Training")
 
+# load dataset
 df = pd.read_csv("train_preprocessed.csv")
 
-X = df.drop("price_range", axis=1)
-y = df["price_range"]
+X = df.drop("target", axis=1)
+y = df["target"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-with mlflow.start_run():
-    model = RandomForestClassifier(
-        n_estimators=100,
-        random_state=42
-    )
-    model.fit(X_train, y_train)
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
 
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
+mlflow.log_metric("accuracy", acc)
+mlflow.sklearn.log_model(model, "model")
